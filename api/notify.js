44d688dc7827e134260eb80bae90ac3032b4b6b6
unknown <pea@C3PSCPT14.pea.co.th>
2026-08-30 09:33:56 +0700

    fix: change ES module to CommonJS for Vercel serverless function

--- api/notify.js
+++ api/notify.js
@@ -1,4 +1,4 @@
-﻿export default async function handler(req, res) {
+﻿module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' });
   }
@@ -36,3 +36,4 @@
     return res.status(500).json({ success: false, error: error.message });
   }
 }
+
